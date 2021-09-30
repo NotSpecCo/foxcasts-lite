b314878a3642231cf653eda78ec537219c9925af
Garrett Downs <garredow@outlook.com>
2021-09-29 21:19:43 -0400

    redesign player view

--- src/routes/Player.tsx
+++ src/routes/Player.tsx
@@ -1,16 +1,23 @@
-import { Chapter } from 'foxcasts-core/lib/types';
+import { Chapter, Podcast } from 'foxcasts-core/lib/types';
 import { formatTime } from 'foxcasts-core/lib/utils';
 import { Fragment, h, VNode } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
-import MiniPlayer from '../components/MiniPlayer';
-import { usePlayer } from '../contexts/playerContext';
+import { PlaybackStatus, usePlayer } from '../contexts/playerContext';
 import { SelectablePriority, useDpad } from '../hooks/useDpad';
+import { useNavKeys } from '../hooks/useNavKeys';
 import { Core } from '../services/core';
 import { ListItem, MenuOption, View } from '../ui-components';
 import styles from './Player.module.css';
 
 export default function Player(): VNode {
   const [chapters, setChapters] = useState<Chapter[] | null>(null);
+  const [podcast, setPodcast] = useState<Podcast | null>(null);
+  const [browsingChapters, setBrowsingChapters] = useState(false);
+  const [status, setStatus] = useState<PlaybackStatus>({
+    playing: false,
+    currentTime: 0,
+    duration: 0,
+  });
 
   const player = usePlayer();
 
@@ -19,6 +26,7 @@ export default function Player(): VNode {
 
     if (!episode) {
       setChapters(null);
+      setPodcast(null);
       return;
     }
 
@@ -31,7 +39,26 @@ export default function Player(): VNode {
         episode.fileUrl
       ).then((res) => setChapters(res));
     }
+
+    Core.getPodcastById(episode.podcastId).then(setPodcast);
+
+    const status = player.getStatus();
+    setStatus(status);
+
+    const timer = setInterval(() => {
+      const status = player.getStatus();
+      setStatus(status);
+    }, 1000);
+
+    return (): void => clearInterval(timer);
   }, [player.episode]);
+
+  useEffect(() => {
+    if (player.episode) {
+      // console.log('update', player.episode.id, status.currentTime);
+      Core.updateEpisode(player.episode.id, { progress: status.currentTime });
+    }
+  }, [player.episode, status.currentTime]);
 
   function handleAction(action: string): void {
     if (!player.episode) {
@@ -80,44 +107,83 @@ export default function Player(): VNode {
   useDpad({
     priority: SelectablePriority.Low,
     onEnter: handleClick,
+    onChange: (itemId) => setBrowsingChapters(!!itemId?.startsWith('chapter')),
+    options: { mode: 'updown' },
   });
+
+  useNavKeys(
+    {
+      ArrowLeft: () => player.jump(-30),
+      ArrowRight: () => player.jump(30),
+      Enter: () => {
+        if (!player.episode) {
+          return;
+        }
+        status.playing ? player.pause() : player.play();
+      },
+    },
+    { disabled: browsingChapters }
+  );
 
   return (
     <View
-      showHeader={true}
+      showHeader={false}
       headerText={player.episode?.podcastTitle || 'Player'}
+      centerMenuText={
+        browsingChapters ? 'Select' : status.playing ? 'Pause' : 'Play'
+      }
       actions={getActionList()}
       onAction={handleAction}
+      backgroundImageUrl={podcast?.artworkUrl}
     >
-      <div
-        data-selectable-priority={SelectablePriority.Low}
-        data-selectable-id="mini-player"
-      >
-        <MiniPlayer />
-      </div>
-      {player.episode && (
+      {player.episode ? (
         <Fragment>
-          <div className={styles.title}>
-            {player.episode && player.episode.title}
+          <div className={styles.player}>
+            <div
+              data-selectable-priority={SelectablePriority.Low}
+              data-selectable-id="top"
+            />
+            <div className={styles.gradient}>
+              <div className={styles.info}>
+                <div className={styles.author}>
+                  {player.episode?.podcastTitle}
+                </div>
+                <div className={styles.title}>{player.episode?.title}</div>
+                <div className={styles.times}>
+                  <span className={styles.currentTime}>
+                    {formatTime(status.currentTime)}
+                  </span>
+                  <span className={styles.duration}>
+                    {` / ${formatTime(status.duration)}`}
+                  </span>
+                  <div className={styles.flex} />
+                  {chapters && chapters.length > 0 ? (
+                    <div className={styles.chevronDown} />
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.chapters}>
+            {chapters?.map((chapter, i) => {
+              let text = formatTime(chapter.startTime / 1000);
+              if (chapter.endTime) {
+                text = `${text} - ${formatTime(chapter.endTime / 1000)}`;
+              }
+              return (
+                <ListItem
+                  key={chapter.startTime}
+                  itemId={`chapter_${i}`}
+                  primaryText={chapter.title}
+                  accentText={text}
+                />
+              );
+            })}
           </div>
         </Fragment>
+      ) : (
+        <div className={styles.message}>Nothing is playing.</div>
       )}
-      {chapters &&
-        chapters.map((chapter, i) => {
-          let text = formatTime(chapter.startTime / 1000);
-          if (chapter.endTime) {
-            text = `${text} - ${formatTime(chapter.endTime / 1000)}`;
-          }
-          return (
-            <ListItem
-              key={chapter.startTime}
-              itemId={`chapter_${i}`}
-              primaryText={chapter.title}
-              accentText={text}
-            />
-          );
-        })}
-      {chapters?.length === 0 ? <div>{player.episode?.description}</div> : null}
     </View>
   );
 }
