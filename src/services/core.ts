fa12afc93b39c2ea5c151f2bdea3928bcbe20fa1
Garrett Downs <garredow@outlook.com>
2021-10-01 12:20:28 -0400

    temporary fix to bypass expired le root cert

--- src/services/core.ts
+++ src/services/core.ts
@@ -1,6 +1,7 @@
 import { FoxcastsCore } from 'foxcasts-core';
 
 export const Core = new FoxcastsCore({
+  baseUrl: 'http://api.foxcasts.com/',
   apiKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZm94Y2FzdHMtbGl0ZSIsImNyZWF0ZWRBdCI6IjIwMjEtMDktMDRUMjE6NDE6MjUuODQxWiIsImlhdCI6MTYzMDc5MTY4NX0.Z9JKUYmPE59Al9q82ctGUg9SeAks9KKOjz-qDm4s3GM',
 });
