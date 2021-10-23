c43e582a78afd4849e4d05910557cb5ce4bd42da
Garrett Downs <garredow@outlook.com>
2021-10-23 15:05:54 -0400

    add app menu list

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -3,15 +3,20 @@ import { route } from 'preact-router';
 import { Typography } from '../ui-components/Typography';
 import styles from './AppMenu.module.css';
 import { Tile } from '../ui-components2/Tile';
-import { SelectablePriority, useDpad } from '../hooks/useDpad';
+import { SelectablePriority } from '../hooks/useDpad';
 import { AppBar } from '../ui-components2/appbar';
 import { useListNav } from '../hooks/useListNav';
+import { useSettings } from '../contexts/SettingsProvider';
+import { ListLayout } from '../models';
+import { SelectableBase } from '../ui-components2/hoc';
+import { SvgIcon } from '../ui-components2/SvgIcon';
 
 interface AppMenuProps {
   onClose: () => void;
 }
 
 export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
+  const { settings } = useSettings();
   function handleSelect(id?: string): void {
     console.log('select', id);
 
@@ -49,73 +54,143 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
     priority: SelectablePriority.Medium,
     onSelect: handleSelect,
   });
-  console.log('selid', selectedId);
 
   return (
     <div className={styles.root}>
       <Typography type="bodyLarge">Foxcasts Lite</Typography>
-      <div className={styles.grid}>
-        <Tile
-          icon="grid"
-          backText="Podcasts"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'podcasts',
-            shortcut: '1',
-            selected: selectedId === 'podcasts',
-          }}
-        />
-        <Tile
-          icon="search"
-          backText="Search"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'search',
-            shortcut: '2',
-            selected: selectedId === 'search',
-          }}
-        />
-        <Tile
-          icon="play"
-          backText="Player"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'player',
-            shortcut: '3',
-            selected: selectedId === 'player',
-          }}
-        />
-        <Tile
-          icon="list"
-          backText="Lists"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'lists',
-            shortcut: '4',
-            selected: selectedId === 'lists',
-          }}
-        />
-        <Tile
-          icon="download"
-          backText="Download"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'downloads',
-            shortcut: '5',
-            selected: selectedId === 'downloads',
-          }}
-        />
-        <Tile
-          icon="settings"
-          backText="Settings"
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'settings',
-            shortcut: '6',
-            selected: selectedId === 'settings',
-          }}
-        />
-      </div>
+      {settings.homeMenuLayout === ListLayout.Grid ? (
+        <div className={styles.grid}>
+          <Tile
+            icon="grid"
+            backText="Podcasts"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'podcasts',
+              shortcut: '1',
+              selected: selectedId === 'podcasts',
+            }}
+          />
+          <Tile
+            icon="search"
+            backText="Search"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'search',
+              shortcut: '2',
+              selected: selectedId === 'search',
+            }}
+          />
+          <Tile
+            icon="play"
+            backText="Player"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'player',
+              shortcut: '3',
+              selected: selectedId === 'player',
+            }}
+          />
+          <Tile
+            icon="list"
+            backText="Lists"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'lists',
+              shortcut: '4',
+              selected: selectedId === 'lists',
+            }}
+          />
+          <Tile
+            icon="download"
+            backText="Download"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'downloads',
+              shortcut: '5',
+              selected: selectedId === 'downloads',
+            }}
+          />
+          <Tile
+            icon="settings"
+            backText="Settings"
+            selectable={{
+              priority: SelectablePriority.Medium,
+              id: 'settings',
+              shortcut: '6',
+              selected: selectedId === 'settings',
+            }}
+          />
+        </div>
+      ) : (
+        <div className={styles.list}>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="podcasts"
+            shortcut="1"
+            selected={selectedId === 'podcasts'}
+          >
+            <span>1</span>
+            <SvgIcon icon="grid" />
+            Podcasts
+          </SelectableBase>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="search"
+            shortcut="2"
+            selected={selectedId === 'search'}
+          >
+            <span>2</span>
+            <SvgIcon icon="search" />
+            Search
+          </SelectableBase>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="player"
+            shortcut="3"
+            selected={selectedId === 'player'}
+          >
+            <span>3</span>
+            <SvgIcon icon="play" />
+            Player
+          </SelectableBase>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="lists"
+            shortcut="4"
+            selected={selectedId === 'lists'}
+          >
+            <span>4</span>
+            <SvgIcon icon="list" />
+            Lists
+          </SelectableBase>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="downloads"
+            shortcut="5"
+            selected={selectedId === 'downloads'}
+          >
+            <span>5</span>
+            <SvgIcon icon="download" />
+            Download
+          </SelectableBase>
+          <SelectableBase
+            className={styles.row}
+            priority={SelectablePriority.Medium}
+            id="settings"
+            shortcut="6"
+            selected={selectedId === 'settings'}
+          >
+            <span>6</span>
+            <SvgIcon icon="settings" />
+            Settings
+          </SelectableBase>
+        </div>
+      )}
       <AppBar leftIcon="cancel" rightIcon={null} />
     </div>
   );
