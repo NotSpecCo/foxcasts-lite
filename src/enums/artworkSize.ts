a18b0faa55e2aa27aee181dfb5115b03f472f87d
Garrett Downs <garredow@outlook.com>
2021-10-23 15:36:20 -0400

    add useArtwork hook

---
+++ src/enums/artworkSize.ts
@@ -0,0 +1,5 @@
+export enum ArtworkSize {
+  Small = 40,
+  Medium = 100,
+  Large = 320,
+}
---
+++ src/hooks/useArtwork.ts
@@ -0,0 +1,43 @@
+import { Artwork } from 'foxcasts-core/lib/types';
+import { useEffect, useState } from 'preact/hooks';
+import { ArtworkBlur } from '../enums/artworkBlur';
+import { ArtworkSize } from '../enums/artworkSize';
+import { Core } from '../services/core';
+
+type Options = {
+  size: ArtworkSize;
+  blur?: ArtworkBlur;
+  greyscale?: boolean;
+};
+
+type Return = {
+  artwork: Artwork | undefined;
+  loading: boolean;
+  error: boolean;
+};
+
+export function useArtwork(
+  podcastId: number | string,
+  { size, blur, greyscale }: Options
+): Return {
+  const [artwork, setArtwork] = useState<Artwork>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+
+    Core.getArtwork(podcastId, { size, blur, greyscale })
+      .then((res) => {
+        setArtwork(res);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, [podcastId, size, blur, greyscale]);
+
+  return { artwork, loading, error };
+}
