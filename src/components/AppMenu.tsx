7393bcb3a49c38e45ee371f52d51ece16463deca
Garrett Downs <garredow@outlook.com>
2021-11-04 15:41:37 -0400

    switch appmenu to single view

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -16,7 +16,7 @@ import { SelectableBase } from '../ui-components/hoc';
 import { SvgIcon } from '../ui-components/SvgIcon';
 import { Tile } from '../ui-components/Tile';
 import { Typography } from '../ui-components/Typography';
-import { ViewTab, ViewTabBar } from '../ui-components/view';
+import { ifClass, joinClasses } from '../utils/classes';
 import styles from './AppMenu.module.css';
 import { ControllerOption } from './ControllerOption';
 
@@ -25,7 +25,6 @@ interface AppMenuProps {
 }
 
 export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
-  const [tabId, setTabId] = useState<'menu' | 'remote'>('menu');
   const [status, setStatus] = useState<PlaybackProgress>({
     playing: false,
     currentTime: 0,
@@ -79,8 +78,11 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
       case 'player':
         pageRoute = '/player/player';
         break;
-      case 'lists':
-        pageRoute = '/lists';
+      case 'playlists':
+        pageRoute = '/playlists';
+        break;
+      case 'filters':
+        pageRoute = '/filters';
         break;
       case 'downloads':
         pageRoute = '/downloads';
@@ -101,18 +103,11 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
 
   return (
     <div className={styles.root}>
-      <Typography type="bodyLarge" padding="horizontal">
-        Foxcasts Lite
-      </Typography>
-      <ViewTabBar
-        tabs={[
-          { id: 'menu', label: 'menu' },
-          { id: 'remote', label: 'remote' },
-        ]}
-        selectedId={tabId}
-        onChange={(tabId) => setTabId(tabId as any)}
-      />
-      <ViewTab tabId="menu" activeTabId={tabId}>
+      <Typography type="bodyLarge">Foxcasts Lite</Typography>
+      <div
+        className={styles.scroller}
+        data-selectable-scroller={SelectablePriority.Medium}
+      >
         {settings.homeMenuLayout === ListLayout.Grid ? (
           <div className={styles.grid}>
             <Tile
@@ -136,23 +131,23 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
               }}
             />
             <Tile
-              icon="play"
-              backText="Player"
+              icon="list"
+              backText="Playlists"
               selectable={{
                 priority: SelectablePriority.Medium,
-                id: 'player',
+                id: 'playlists',
                 shortcut: '3',
-                selected: selectedId === 'player',
+                selected: selectedId === 'playlists',
               }}
             />
             <Tile
-              icon="list"
-              backText="Lists"
+              icon="filter"
+              backText="Filters"
               selectable={{
                 priority: SelectablePriority.Medium,
-                id: 'lists',
+                id: 'filters',
                 shortcut: '4',
-                selected: selectedId === 'lists',
+                selected: selectedId === 'filters',
               }}
             />
             <Tile
@@ -185,8 +180,8 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
               shortcut="1"
               selected={selectedId === 'podcasts'}
             >
-              <span>1</span>
               <SvgIcon icon="grid" />
+              <span>1</span>
               Podcasts
             </SelectableBase>
             <SelectableBase
@@ -196,31 +191,31 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
               shortcut="2"
               selected={selectedId === 'search'}
             >
-              <span>2</span>
               <SvgIcon icon="search" />
+              <span>2</span>
               Search
             </SelectableBase>
             <SelectableBase
               className={styles.row}
               priority={SelectablePriority.Medium}
-              id="player"
+              id="playlists"
               shortcut="3"
-              selected={selectedId === 'player'}
+              selected={selectedId === 'playlists'}
             >
+              <SvgIcon icon="list" />
               <span>3</span>
-              <SvgIcon icon="play" />
-              Player
+              Playlists
             </SelectableBase>
             <SelectableBase
               className={styles.row}
               priority={SelectablePriority.Medium}
-              id="lists"
+              id="filters"
               shortcut="4"
-              selected={selectedId === 'lists'}
+              selected={selectedId === 'filters'}
             >
+              <SvgIcon icon="filter" />
               <span>4</span>
-              <SvgIcon icon="list" />
-              Lists
+              Filters
             </SelectableBase>
             <SelectableBase
               className={styles.row}
@@ -229,8 +224,8 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
               shortcut="5"
               selected={selectedId === 'downloads'}
             >
-              <span>5</span>
               <SvgIcon icon="download" />
+              <span>5</span>
               Download
             </SelectableBase>
             <SelectableBase
@@ -240,66 +235,84 @@ export function AppMenu(props: AppMenuProps): h.JSX.Element | null {
               shortcut="6"
               selected={selectedId === 'settings'}
             >
-              <span>6</span>
               <SvgIcon icon="settings" />
+              <span>6</span>
               Settings
             </SelectableBase>
           </div>
         )}
-      </ViewTab>
-      <ViewTab tabId="remote" activeTabId={tabId}>
-        <Typography type="caption">Now Playing</Typography>
         {player.episode ? (
           <Fragment>
-            <Typography padding="horizontal">
-              {player.episode?.title}
-            </Typography>
-            <Typography padding="horizontal" color="accent">
-              {player.episode?.podcastTitle}
-            </Typography>
-            <Typography padding="horizontal" display="inline" type="bodyLarge">
-              {formatTime(status.currentTime || 0)}
-            </Typography>
-            {' / '}
-            <Typography padding="horizontal" display="inline" color="secondary">
-              {formatTime(status.duration || 0)}
-            </Typography>
-          </Fragment>
-        ) : (
-          <Typography>Nothing playing</Typography>
-        )}
-        <Typography type="caption">Controls</Typography>
-        <ControllerOption
-          label="Playback"
-          disabled={!player.episode}
-          leftAction={() => setStatus(player.jump(-settings.playbackSkipBack))}
-          rightAction={() =>
-            setStatus(player.jump(settings.playbackSkipForward))
-          }
-          centerAction={() =>
-            status.playing
-              ? setStatus(player.pause())
-              : setStatus(player.play())
-          }
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'playback',
-            selected: selectedId === 'playback',
-          }}
-        />
-        <ControllerOption
-          label="Volume"
-          leftAction={() => KaiOS.system.volumeDown()}
-          rightAction={() => KaiOS.system.volumeUp()}
-          centerAction={() => KaiOS.system.volumeShow()}
-          selectable={{
-            priority: SelectablePriority.Medium,
-            id: 'volume',
-            selected: selectedId === 'volume',
-          }}
-        />
-      </ViewTab>
+            {settings.homeMenuLayout === ListLayout.List && (
+              <Typography type="caption">Now Playing</Typography>
+            )}
+            <SelectableBase
+              className={joinClasses(
+                styles.player,
+                ifClass(selectedId === 'player', styles.selected)
+              )}
+              id="player"
+              priority={SelectablePriority.Medium}
+              shortcut="7"
+            >
+              <Typography padding="horizontal" wrap="nowrap">
+                {player.episode?.title}
+              </Typography>
+              <Typography padding="horizontal" color="secondary" wrap="nowrap">
+                {player.episode?.podcastTitle}
+              </Typography>
+              <Typography
+                padding="horizontal"
+                display="inline"
+                type="bodyStrong"
+              >
+                {formatTime(status.currentTime || 0)}
+              </Typography>
+              /
+              <Typography
+                padding="horizontal"
+                display="inline"
+                color="secondary"
+              >
+                {formatTime(status.duration || 0)}
+              </Typography>
+            </SelectableBase>
 
+            <Typography type="caption">Controls</Typography>
+            <ControllerOption
+              label="Playback"
+              disabled={!player.episode}
+              leftAction={() =>
+                setStatus(player.jump(-settings.playbackSkipBack))
+              }
+              rightAction={() =>
+                setStatus(player.jump(settings.playbackSkipForward))
+              }
+              centerAction={() =>
+                status.playing
+                  ? setStatus(player.pause())
+                  : setStatus(player.play())
+              }
+              selectable={{
+                priority: SelectablePriority.Medium,
+                id: 'playback',
+                selected: selectedId === 'playback',
+              }}
+            />
+            <ControllerOption
+              label="Volume"
+              leftAction={() => KaiOS.system.volumeDown()}
+              rightAction={() => KaiOS.system.volumeUp()}
+              centerAction={() => KaiOS.system.volumeShow()}
+              selectable={{
+                priority: SelectablePriority.Medium,
+                id: 'volume',
+                selected: selectedId === 'volume',
+              }}
+            />
+          </Fragment>
+        ) : null}
+      </div>
       <AppBar leftIcon="cancel" rightIcon={null} />
     </div>
   );
